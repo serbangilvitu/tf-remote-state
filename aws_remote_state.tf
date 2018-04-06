84569e72d8fcc950d9a7837b594fb9845fd448ca
Serban Gilvitu <>
2018-04-06 11:16:14 +0100

    Ignore *.tfstate

--- aws_remote_state.tf
+++ aws_remote_state.tf
@@ -14,9 +14,12 @@ provider "aws" {
   region     = "${var.aws_region}"
 }
 
-
 resource "aws_s3_bucket" "b" {
   bucket = "${var.s3_bucket}"
   acl    = "private"
   tags = "${var.s3_bucket_tags}"
+}
+
+output "s3_backend_id" {
+  value = "${aws_eip.ip.public_ip}"
 }
